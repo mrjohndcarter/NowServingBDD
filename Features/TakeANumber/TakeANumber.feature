8df61696f36298c49e7928a90f249edcc82f5ef9
John Carter <john.carter@youi.tv>
2016-11-01 15:49:21 -0400

    Implemented all steps for TakeANumber

--- Features/TakeANumber/TakeANumber.feature
+++ Features/TakeANumber/TakeANumber.feature
@@ -1,7 +1,6 @@
 Feature: Ticketing
   As a business owner, I want to effectively manage customers waiting to be served in a "first come, first served" manner.
 
-  @wip
   Scenario: Start_Machine
     Given Machine is off
     When  Machine is started
@@ -18,13 +17,13 @@ Feature: Ticketing
   Scenario: Customer_Takes_Ticket
     Given Machine is on
     When  Customer takes <ticket>
-    Then  <ticket> should be less than or equal to the display
+    Then  Display should be less than or equal to the ticket
 
   Scenario: Serve
     Given Machine is on
     And   Display is less than <ticket>
     When  A customer is called
-    Then  Display should increase by 1
+    Then  Display should be less than or equal to the ticket
 
   Scenario: Reset_Machine
     Given Machine is on
--- Features/TakeANumber/steps/TakeANumber-steps.py
+++ Features/TakeANumber/steps/TakeANumber-steps.py
@@ -21,125 +21,48 @@ def step_impl(context):
 
 @then(u'Display shows 1')
 def step_impl(context):
-    context.state.test_that(context, 'display', '=', 1)
+    context.state.test_that(context, 'display', '=', '1')
 
 @then(u'Ticket is 1')
 def step_impl(context):
-    context.state.test_that(context, 'ticket', '=', 1)
+    context.state.test_that(context, 'ticket', '=', '1')
 
 @given(u'Machine is on')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given Machine is on')
+    context.state.ensure_that('running', '=', 'TRUE')
 
 @when(u'Machine is stopped')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When Machine is stopped')
+    context.state.assign([('running', ':=', 'FALSE')])
+    context.state.assign([('display', ':=', '0')])
 
 @then(u'Machine is off')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then Machine is off')
+    context.state.test_that(context, 'running', '=', 'FALSE')
 
 @then(u'Display shows 0')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then Display shows 0')
+    context.state.test_that(context, 'display', '=', '0')
 
 @when(u'Customer takes <ticket>')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When Customer takes <ticket>')
+    context.state.assign([('ticket', ':=', 'ticket + 1')])
 
-@then(u'<ticket> should be less than or equal to the display')
+@then(u'Display should be less than or equal to the ticket')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then <ticket> should be less than or equal to the display')
+    context.state.test_that(context, 'display', '<=', 'ticket')
+    context.state.test_that_always(context, 'display', '<=', 'ticket')
 
 @given(u'Display is less than <ticket>')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given Display is less than <ticket>')
+    context.state.ensure_that('display', '<', 'ticket')
 
 @when(u'A customer is called')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When A customer is called')
-
-@then(u'Display should increase by 1')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then Display should increase by 1')
+    context.state.assign([('display', ':=', 'display + 1')])
 
 @when(u'Machine is reset')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When Machine is reset')
+    context.state.assign([('display', ':=', '1')])
+    context.state.assign([('ticket', ':=', '1')])
 
-# @given(u'Machine is on')
-# def step_impl(context):
-#     context.state.ensure_that('running', '=', 'TRUE')
-#
-#
-# @when(u'Machine is stopped')
-# def step_impl(context):
-#     context.state.assign([('running', ':=', 'FALSE')])
-#
-#
-# @then(u'Machine is off')
-# def step_impl(context):
-#     context.state.test_that(context, 'running', '=', 'TRUE')
-#
-# @then(u'Display should be empty')
-# def step_impl(context):
-#     pass
-
-# @then(u'The next ticket should be 1')
-# def step_impl(context):
-#     # bhive needs to know what?  this is an assertion of the machine state
-#     assert context.machine.take_ticket() == 1
-#
-#
-# @then(u'The "Now Serving" display should be blank')
-# def step_impl(context):
-#     assert context.machine.display == ''
-#
-# @given(u'Machine is on')
-# def step_impl(context):
-#     raise NotImplementedError(u'STEP: Given Machine is on')
-#
-#
-# @when(u'A ticket is dispensed')
-# def step_impl(context):
-#     raise NotImplementedError(u'STEP: When A ticket is dispensed')
-#
-#
-# @then(u'The ticket should be less than the displayed number')
-# def step_impl(context):
-#     raise NotImplementedError(u'STEP: Then The ticket should be less than the displayed number')
-#
-#
-# @given(u'Wicket <wicket> is open')
-# def step_impl(context):
-#     raise NotImplementedError(u'STEP: Given Wicket <wicket> is open')
-#
-#
-# @when(u'Wicket <wicket> serves a customer with ticket <ticket>')
-# def step_impl(context):
-#     raise NotImplementedError(u'STEP: When Wicket <wicket> serves a customer with ticket <ticket>')
-#
-#
-# @then(u'The "Now Serving" display should show <wicket> is serving <ticket>')
-# def step_impl(context):
-#     raise NotImplementedError(u'STEP: Then The "Now Serving" display should show <wicket> is serving <ticket>')
-#
-#
-# @when(u'Wicket <wicket> is closed')
-# def step_impl(context):
-#     raise NotImplementedError(u'STEP: When Wicket <wicket> is closed')
-#
-#
-# @then(u'The "Now Serving" display should now show <wicket>')
-# def step_impl(context):
-#     raise NotImplementedError(u'STEP: Then The "Now Serving" display should now show <wicket>')
-#
-#
-# @when(u'Machine is stopped')
-# def step_impl(context):
-#     raise NotImplementedError(u'STEP: When Machine is stopped')
-#
-#
-# @then(u'Machine is off')
-# def step_impl(context):
-#     raise NotImplementedError(u'STEP: Then Machine is off')
