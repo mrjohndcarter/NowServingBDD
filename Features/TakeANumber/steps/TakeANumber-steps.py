a39d378cbc7e0d3b0b37c66cb489c83022124253
John Carter <john.carter@youi.tv>
2017-02-21 20:25:54 -0500

    Updated take a number to latest bhive

--- Features/TakeANumber/steps/TakeANumber-steps.py
+++ Features/TakeANumber/steps/TakeANumber-steps.py
@@ -6,15 +6,15 @@ from bhive.integration import log_info, declare_variable
 def step_impl(context):
     # start bhive
     declare_variable(context, 'running', 'BOOL', 'FALSE')
-    declare_variable(context, 'display', 'INT', '0')
-    declare_variable(context, 'ticket', 'INT', '0')
-    context.state.ensure_that('running','=','FALSE')
+    context.state.ensure_that(context, 'running','=','FALSE')
     # end bhive
     context.domain_model.running = False
 
 @when(u'Machine is started')
 def step_impl(context):
     # start bhive
+    declare_variable(context, 'display', 'INT', '0')
+    declare_variable(context, 'ticket', 'INT', '0')
     context.state.assign([('running', ':=', 'TRUE')])
     context.state.assign([('display', ':=', '0')])
     context.state.assign([('ticket', ':=', '1')])
@@ -28,7 +28,7 @@ def step_impl(context):
     # end bhive
     assert(context.domain_model.running is True)
 
-@then(u'Display shows 1')
+@then(u'Display shows 0')
 def step_impl(context):
     # start bhive
     context.state.test_that(context, 'display', '=', '0')
@@ -45,7 +45,7 @@ def step_impl(context):
 @given(u'Machine is on')
 def step_impl(context):
     # start bhive
-    context.state.ensure_that('running', '=', 'TRUE')
+    context.state.ensure_that(context, 'running', '=', 'TRUE')
     # end bhive
     context.domain_model.running = True
 
@@ -63,13 +63,6 @@ def step_impl(context):
     context.state.test_that(context, 'running', '=', 'FALSE')
     # end bhive
     assert(context.domain_model.running is False)
-
-@then(u'Display shows 0')
-def step_impl(context):
-    # start bhive
-    context.state.test_that(context, 'display', '=', '0')
-    # end bhive
-    assert(context.domain_model.display == 0)
 
 @when(u'Customer takes <ticket>')
 def step_impl(context):
@@ -89,7 +82,7 @@ def step_impl(context):
 @given(u'Display is less than <ticket>')
 def step_impl(context):
     # start bhive
-    context.state.ensure_that('display', '<', 'ticket')
+    context.state.ensure_that(context, 'display', '<', 'ticket')
     # end bhive
     context.domain_model.display = 5
     context.domain_model.ticket = 7
